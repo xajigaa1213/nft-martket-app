37475b32bb5455189729f7489089b8f49d2539f1
zakariye <rashka@gmail.com>
2023-02-05 08:33:08 +0300

    done

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+import 'package:nft_marketplace/scrn/introduce_screen.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'NFT Marketplace',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const IntroduceScreen(),
+    );
+  }
+}
